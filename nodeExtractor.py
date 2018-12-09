329d7ee9d8dd9758aaba55bbb45f1fa9333bcc92
timolex <timo.surbeck@gmail.com>
2018-12-09 18:37:30 +0100

    Filtering using sine wave between transmissions (part 1)

--- nodeExtractor.py
+++ nodeExtractor.py
@@ -3,6 +3,7 @@ import s2sphere  # https://s2sphere.readthedocs.io/en/latest/index.html
 import math
 import datetime
 import numpy
+import matplotlib.pyplot as plot
 
 EARTH_RADIUS = 6371000
 
@@ -58,40 +59,92 @@ with open('test.csv', 'r', encoding='unicode_escape') as csv_file:
 # filtering out non-periodically-sending nodes
 nodesToRemove = []
 for node in nodeDict:
-	# calculating the time-difference between first and last transmission in seconds
-	timeDiff = nodeDict[node].__getitem__(len(nodeDict[node]) - 1).time_stamp - nodeDict[node].__getitem__(0).time_stamp
-	# initializing the periodicity table (list)
-	periodicityTable = []
-	# initializing the counters
-	secondCount = 0
-	packetCount = 0
-	startSecond = nodeDict[node].__getitem__(0).time_stamp
-	# building the table
-	while packetCount < len(nodeDict[node]) and secondCount <= timeDiff:
-		if startSecond + secondCount == nodeDict[node].__getitem__(packetCount).time_stamp:
-			periodicityTable.append(1)
-			packetCount = packetCount + 1
-		else:
-			periodicityTable.append(0)
-		secondCount = secondCount + 1
+	if len(nodeDict[node]) > 5:
+		# calculating the time-difference between first and last transmission in seconds
+		timeDiff = nodeDict[node].__getitem__(len(nodeDict[node]) - 1).time_stamp - nodeDict[node].__getitem__(
+			0).time_stamp
+		# initializing the periodicity table (list)
+		periodicityTable = []
+		# initializing the counters
+		secondCount = 0
+		packetCount = 0
+		startSecond = nodeDict[node].__getitem__(0).time_stamp
+		# building the table
+		while packetCount < len(nodeDict[node]) and secondCount <= timeDiff:
+			if startSecond + secondCount == nodeDict[node].__getitem__(packetCount).time_stamp:
+				periodicityTable.append(1)
+				# jumping over packetTransmissions in the same second
+				while packetCount < len(nodeDict[node]) and \
+						startSecond + secondCount == nodeDict[node].__getitem__(packetCount).time_stamp:
+					packetCount = packetCount + 1
+			else:
+				periodicityTable.append(0)
+			secondCount = secondCount + 1
 
-	fftPeriodicityTable = numpy.abs(numpy.fft.rfft(periodicityTable))
+		if len(periodicityTable) > 1:
+			sinePeriodicity = []
+			for i in range(len(nodeDict[node]) - 1):
+				j = i + 1
+				interval = int(
+					nodeDict[node].__getitem__(j).__getattribute__('time_stamp') - nodeDict[node].__getitem__(
+						i).__getattribute__("time_stamp"))
+				sinePeriodicity.append(0)
+				for q in range(interval - 1):
+					sinePeriodicity.append(numpy.sin(2 * numpy.pi * (q + 1) / (interval + 1)))
 
-	peakFound = False
-	i = 1
+			fftPeriodicityTable = numpy.abs(numpy.fft.rfft(periodicityTable))
+			fftSinTable = numpy.abs(numpy.fft.rfft(sinePeriodicity))
 
-	# TODO: Should there be a maximum for periodicities like implemented below?
-	# filtering the nodes, while ignoring periodicities of greater than one week
-	while peakFound is False and i < len(fftPeriodicityTable) and i < 606000:
-		if fftPeriodicityTable[i] / fftPeriodicityTable[0] > 0.6:
-			peakFound = True
-		i = i+1
+			if node == '0x01010601':
+				plot.plot(periodicityTable)
+				plot.title("periodicityTable")
+				plot.xlabel("seconds (between first and last transmission of node)")
+				plot.ylabel("1 = packet sent, 0 = no packet sent")
+				plot.show()
+				plot.plot(sinePeriodicity)
+				plot.title("sine version of periodicityTable")
+				plot.xlabel("seconds (between first and last transmission of node)")
+				plot.ylabel("sine wave, one full period between two transmissions")
+				print('Max of fftSinTable:')
+				print(max(fftSinTable))
+				print('length of periodicityTable / 2:')
+				print(len(periodicityTable) / 2)
+				print('ratio max/len:')
+				print(str(max(fftSinTable) / (len(periodicityTable) / 2)))
+				plot.show()
+				plot.plot(fftSinTable)
+				plot.title("fft on sin")
+				plot.xlabel("seconds (between first and last transmission of node)")
+				plot.show()
+				plot.plot(fftPeriodicityTable)
+				plot.title("fft on periodicityTable")
+				plot.xlabel("seconds (between first and last transmission of node)")
+				plot.show()
 
-	# remove the node if no peak greater than 0.6*fft[0] was detected
-	if not peakFound:
-		# print the node to be filtered out
+			if not (max(fftSinTable) / (len(periodicityTable) / 2)) > 0.4:
+				# print the node to be filtered out
+				print(node)
+				nodesToRemove.append(node)
+	else:
+		# print the node to be filtered out due to too little transmissions
 		print(node)
 		nodesToRemove.append(node)
+
+	# peakFound = False
+		# i = 1
+		#
+		# # TODO: Should there be a maximum for periodicities like implemented below?
+		# # filtering the nodes, while ignoring periodicities of greater than one week
+		# while peakFound is False and i < len(fftPeriodicityTable) and i < 606000:
+		# 	if fftPeriodicityTable[i] / fftPeriodicityTable[0] > 0.6:
+		# 		peakFound = True
+		# 	i = i+1
+		#
+		# # remove the node if no peak greater than 0.6*fft[0] was detected
+		# if not peakFound:
+		# 	# print the node to be filtered out
+		# 	print(node)
+		# 	nodesToRemove.append(node)
 
 # removing all non-periodically-sending nodes from the nodeDict
 for node in nodesToRemove:
