00bdfa49621c12c4ec779cc4729111b9ecadc626
timolex <timo.surbeck@gmail.com>
2018-12-28 17:32:02 +0100

    Mostafa's method part 1

--- nodeExtractor.py
+++ nodeExtractor.py
@@ -8,14 +8,18 @@ import time
 import pyproj as proj
 
 # 'program parameters:'
-TESTING_MODE = True
-MINIMUM_PEAK_TO_TRANSMISSIONS_RATIO = 0.4
+TESTING_MODE = False
+MINIMUM_PEAK_TO_TRANSMISSIONS_RATIO = 0.5
 # this value has to be at least 2 for fft to work properly
-MINIMUM_NO_OF_PACKETS_SENT = 2
+MINIMUM_NO_OF_PACKETS_SENT = 100
 # criterion for filtering out short-living nodes
+# This value must be in between the periodicity boundaries used for Mostafa's method
 # MINIMUM_LIFESPAN = 2592000 # 30 days in seconds
-# MINIMUM_LIFESPAN = 86400  # 24 h in seconds
-MINIMUM_LIFESPAN = 200  # for testing
+# MINIMUM_LIFESPAN = 86400  # 24h in seconds
+MINIMUM_LIFESPAN = 8000  # for testing
+# periodicity boundaries used for the frequency cutoff in Mostafa's method
+UPPER_BOUND_PERIODICITY = 7200  # 2h in seconds
+LOWER_BOUND_PERIODICITY = 1209600  # 2 weeks in seconds
 
 EARTH_RADIUS = 6371000
 
@@ -31,7 +35,7 @@ if TESTING_MODE:
 	region = s2sphere.LatLngRect.from_point_pair(southWest, northEast)
 else:
 	# setting the filename
-	filename = "packets.csv"
+	filename = "1M.csv"
 
 	# defining the center of the city of Zurich and the radius of the cap to be drawn around it
 	centerOfZurich = s2sphere.LatLng.from_degrees(47.37174, 8.54226)
@@ -77,38 +81,42 @@ with open(filename, 'r', encoding='unicode_escape') as csv_file:
 			timeStamp = datetime.datetime.strptime(line[1], '%Y-%m-%d %H:%M:%S').timestamp()
 			nodeDict.get(line[2]).append(PacketTransmission(line[0], timeStamp, line[10], line[11]))
 
-keptNodesLifespanCheck = []
-shortLivingNodes = []
-keptNodesMethodTimo = []
-remainderMethodTimo = []
-keptNodesMethodMostafa = []
-remainderMethodMostafa = []
-keptNodesMethodMachineLearning = []
-remainderMethodMachineLearning = []
+keptNodesLifespanCheck = {}
+shortLivingNodes = {}
+keptNodesMethodTimo = {}
+remainderMethodTimo = {}
+keptNodesMethodMostafa = {}
+remainderMethodMostafa = {}
+keptNodesMethodMachineLearning = {}
+remainderMethodMachineLearning = {}
 
 # filtering out short-living nodes, resp. nodes with too little transmissions
 for node in nodeDict:
 	# calculating the time-difference between first and last transmission in seconds
-	timeDiff = nodeDict[node].__getitem__(len(nodeDict[node]) - 1).time_stamp - nodeDict[node].__getitem__(0).time_stamp
+	timeSpan = nodeDict[node].__getitem__(len(nodeDict[node]) - 1).time_stamp - nodeDict[node].__getitem__(0).time_stamp
 	packetLength = len(nodeDict[node])
 
-	if packetLength < MINIMUM_NO_OF_PACKETS_SENT or timeDiff < MINIMUM_LIFESPAN:
-		shortLivingNodes.append(node)
+	if packetLength < MINIMUM_NO_OF_PACKETS_SENT:
+		shortLivingNodes[node] = nodeDict[node]
+		print('Failing lifespan check: ' + node + ' (reason: too little transmissions: '
+								+ str(packetLength) + ' packets transmitted)')
+	elif timeSpan < MINIMUM_LIFESPAN:
+		shortLivingNodes[node] = nodeDict[node]
 		print('Failing lifespan check: ' + node + ' (reason: lifespan between 1st and last transmission too short: '
-								+ str(timeDiff) + ' s)')
+								+ str(timeSpan) + ' s)')
 	else:
-		keptNodesLifespanCheck.append(node)
+		keptNodesLifespanCheck[node] = nodeDict[node]
 
 # filtering after Timo's method (sine period between transmissions, determine strong single frequencies)
 for node in keptNodesLifespanCheck:
 	# building the sine list
 	sinePeriodicity = []
-	for i in range(len(nodeDict[node]) - 1):
+	for i in range(len(keptNodesLifespanCheck[node]) - 1):
 		# determining the next interval (time in seconds between two transmissions)
 		j = i + 1
 		interval = int(
-			nodeDict[node].__getitem__(j).__getattribute__('time_stamp') - nodeDict[node].__getitem__(
-				i).__getattribute__("time_stamp"))
+			keptNodesLifespanCheck[node].__getitem__(j).__getattribute__('time_stamp')
+			- keptNodesLifespanCheck[node].__getitem__(i).__getattribute__("time_stamp"))
 		# adding 0 for sin(0) per default
 		sinePeriodicity.append(0)
 		# appending the y-values for one cycle of a sine wave which spans the current interval
@@ -122,11 +130,11 @@ for node in keptNodesLifespanCheck:
 	# (e.g., their peak/transmissions-ratio is too low)
 	ratio = max(fftSinTable) / (len(fftSinTable))
 	if ratio < MINIMUM_PEAK_TO_TRANSMISSIONS_RATIO:
-		remainderMethodTimo.append(node)
+		remainderMethodTimo[node] = keptNodesLifespanCheck[node]
 		print('Failing Timo\'s method: ' + node + ' (reason: peak/transmissions-ratio too low: ' + str(ratio) + ')')
 	else:
+		keptNodesMethodTimo[node] = keptNodesLifespanCheck[node]
 		# printing the peak periodicity (by converting the found peak frequency)
-		keptNodesMethodTimo.append(node)
 		print('Node ' + node + ' is most regularly transmitting all '
 								+ str(len(sinePeriodicity) / numpy.argmax(fftSinTable)) + ' seconds.')
 
@@ -143,21 +151,22 @@ for node in keptNodesLifespanCheck:
 
 # filtering after Mostafa's method
 for node in remainderMethodTimo:
-	timeDiff = nodeDict[node].__getitem__(len(nodeDict[node]) - 1).time_stamp - nodeDict[node].__getitem__(0).time_stamp
+	timeSpan = remainderMethodTimo[node].__getitem__(len(remainderMethodTimo[node]) - 1).time_stamp \
+					- remainderMethodTimo[node].__getitem__(0).time_stamp
 	# initializing the counters
 	secondCount = 0
 	packetCount = 0
-	startSecond = nodeDict[node].__getitem__(0).time_stamp
+	startSecond = remainderMethodTimo[node].__getitem__(0).time_stamp
 	# building the periodicityTable
 	# initializing the periodicity table (list)
 	periodicityTable = []
-	while packetCount < len(nodeDict[node]) and secondCount <= timeDiff:
-		if startSecond + secondCount == nodeDict[node].__getitem__(packetCount).time_stamp:
+	while packetCount < len(remainderMethodTimo[node]) and secondCount <= timeSpan:
+		if startSecond + secondCount == remainderMethodTimo[node].__getitem__(packetCount).time_stamp:
 			# appending 1 to the periodicityTable to signalize a transmission at the current second
 			periodicityTable.append(1)
 			# skipping packetTransmissions in the same second
-			while packetCount < len(nodeDict[node]) and \
-					startSecond + secondCount == nodeDict[node].__getitem__(packetCount).time_stamp:
+			while packetCount < len(remainderMethodTimo[node]) and \
+					startSecond + secondCount == remainderMethodTimo[node].__getitem__(packetCount).time_stamp:
 				packetCount = packetCount + 1
 		else:
 			# appending 0 to the periodicityTable if no transmission happened at current second
@@ -167,8 +176,56 @@ for node in remainderMethodTimo:
 	# computing fft for periodicityTable
 	fftPeriodicityTable = numpy.abs(numpy.fft.rfft(periodicityTable))
 
+	# converting the provided periodicity-cutoffs to the looked-at node's time domain
+	if timeSpan < UPPER_BOUND_PERIODICITY:
+		raise ValueError('Node\'s lifespan must strictly be greater than the lower bound periodicity!')
+	else:
+		upperBoundFrequency = int(round(timeSpan / UPPER_BOUND_PERIODICITY))
+		if upperBoundFrequency > len(fftPeriodicityTable):
+			upperBoundFrequency = len(fftPeriodicityTable-1)
+
+	if timeSpan < LOWER_BOUND_PERIODICITY:
+		lowerBoundFrequency = 1
+	else:
+		lowerBoundFrequency = int(round(timeSpan / LOWER_BOUND_PERIODICITY))
+
+	# determining the peak frequency using the frequency-cutoff
+	peakFrequencyY = 0
+	peakFrequencyX = 0
+	for i in range(lowerBoundFrequency, upperBoundFrequency):
+		if fftPeriodicityTable[i] > peakFrequencyY:
+			peakFrequencyY = fftPeriodicityTable[i]
+			peakFrequencyX = i
+
+	# converting the found peakFrequency to periodicity
+	peakPeriodicity = int(round(timeSpan / peakFrequencyX))
+
+	# checking back, if found peakPeriodicity appears frequently in periodicityTable
+	intervalIndex = 0
+	intervalCounter = 0
+	intervalCountList = []
+	for j in range(len(periodicityTable)):
+		if periodicityTable[j] == 1:
+			intervalCounter = intervalCounter + 1
+		if intervalIndex == peakPeriodicity - 1:
+			if intervalCounter != 0:
+				intervalCountList.append(intervalCounter)
+			intervalIndex = 0
+			intervalCounter = 0
+		intervalIndex = intervalIndex + 1
+
+	# keep the node, if for at least 0.7% of the intervals (acc. to peakPeriodicity) there appeared transmissions
+	if len(intervalCountList) > 0.7 * peakFrequencyX:
+		print('Node ' + node + ' has been verified to be transmitting regularly all '
+								+ str(peakPeriodicity) + ' seconds.')
+		keptNodesMethodMostafa[node] = remainderMethodTimo[node]
+	else:
+		print('Failing Mostafa\'s method: ' + node + ' (reason: intervals/peakFrequency-ratio too low.')
+		remainderMethodMostafa[node] = remainderMethodTimo[node]
+
 # filtering after machine learning methods
 # for node in remainderMethodMostafa: TODO: implementation
+
 
 # printing the number of found end devices in the area
 print("\n# of found suitable end devices in the defined area: " + str(len(keptNodesMethodTimo)
@@ -177,7 +234,13 @@ print("\n# of found suitable end devices in the defined area: " + str(len(keptNo
 # printing nodes with regular traffic (after Timo's method)
 print('')
 for i in keptNodesMethodTimo:
-	print("To be considered \"" + i + "\", number of packets: " + str(len(nodeDict[i])))
+	print("To be considered \"" + i + "\", number of packets: " + str(len(keptNodesMethodTimo[i])))
+
+# printing nodes with regular traffic (after Mostafa's method)
+print('')
+for i in keptNodesMethodMostafa:
+	print("To be considered \"" + i + "\", number of packets: " + str(len(keptNodesMethodMostafa[i])))
+
 
 # setting up projections (according to Coordinate Reference Systems WGS84 (lat.-lon.) and CH1903 (Switzerland))
 proj_WGS84 = proj.Proj(init='epsg:4326')
@@ -186,10 +249,17 @@ proj_CH1903 = proj.Proj(init='epsg:21781')  # http://epsg.io/21781
 # iterating over keptNodesMethodTimo, converting coordinates to epsg:21781-projection
 print('')
 for node in keptNodesMethodTimo:
-	lon = nodeDict[node].__getitem__(0).__getattribute__('lon')
-	lat = nodeDict[node].__getitem__(0).__getattribute__('lat')
+	lon = keptNodesMethodTimo[node].__getitem__(0).__getattribute__('lon')
+	lat = keptNodesMethodTimo[node].__getitem__(0).__getattribute__('lat')
 	x, y = proj.transform(proj_WGS84, proj_CH1903, lon, lat)
 	print(node + ' x: ' + str(x) + ', y: ' + str(y))
+
+for node in keptNodesMethodMostafa:
+	lon = keptNodesMethodMostafa[node].__getitem__(0).__getattribute__('lon')
+	lat = keptNodesMethodMostafa[node].__getitem__(0).__getattribute__('lat')
+	x, y = proj.transform(proj_WGS84, proj_CH1903, lon, lat)
+	print(node + ' x: ' + str(x) + ', y: ' + str(y))
+
 
 # stopping the timer:
 time_stop = time.clock()
