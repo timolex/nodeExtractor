02eb68a93561e29459b7a5f54026740e21100d65
timolex <timo.surbeck@gmail.com>
2019-04-05 11:45:15 +0200

    Yet some more refinements to setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "=========== NodeExtractor python setup ==========="
-echo "Creating virtual environment for python3.6 directory venv/ ..."
+echo -e "\nCreating virtual environment for python3.6 directory venv/ ..."
 mkdir venv
 virtualenv -q -p /usr/bin/python3.6 venv/
-echo "Virtual environment successfully set up!"
+echo -e "Virtual environment successfully set up!\n"
 source venv/bin/activate
-echo "Downloading & installing python dependencies ..."
+echo -e "Downloading & installing python dependencies ...\n"
 pip install s2sphere datetime matplotlib pyproj
-echo "Please check, if pip did not raise any errors while installing the following dependencies:"
+echo -e "\nPlease check, if pip did not raise any errors while installing the following dependencies:"
 echo "s2sphere datetime matplotlib pyproj"
-echo "====== NodeExtractor python setup complete! ======"
-echo "Please run '$ source venv/bin/activate' and '$ python3.6 nodeExtractor.py' to run the application."
+echo -e "\n====== NodeExtractor python setup complete! ======\n"
+echo "Please execute '$ source venv/bin/activate' and then '$ python3.6 nodeExtractor.py' to run the application."
